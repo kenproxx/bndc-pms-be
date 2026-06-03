7f662f54bba1fc0bf729de7f349ea6cc5ff80675
Ken <kienhue98@gmail.com>
2026-06-03 19:40:06 +0700

    304 -> 200

--- app.js
+++ app.js
@@ -26,9 +26,14 @@ var ensureAuth = require('./src/middleware/auth').ensureAuth;
 var app = express();
 
 app.disable('x-powered-by');
+app.disable('etag');
 
 app.use(logger(config.nodeEnv === 'production' ? 'combined' : 'dev'));
 app.use(cors(config.cors));
+app.use(function(req, res, next) {
+  res.set('Cache-Control', 'no-store');
+  return next();
+});
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
